d06d5ee7e233db7d6372d93228fe399bb79ae41b
AneeqBass <aneeqbass@gmail.com>
2024-09-03 10:11:07 +0200

    Added basic website skeleton

---
+++ src/views/AdminView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container admin">
+      <AdminProducts />
+      <AdminUsers />
+    </div>
+  </template>
+  
+  <script>
+  import AdminProducts from '@/components/AdminProducts.vue';
+  import AdminUsers from '@/components/AdminUsers.vue';
+  
+  export default {
+  components: {
+    AdminProducts,
+    AdminUsers,
+  },
+  };
+  </script>
+  
+  <style scoped>
+  .admin {
+    line-height: 1.5;
+    font-size: 17px;
+    letter-spacing: 0.05em;
+    margin-top: 120px;
+  }
+  </style>
