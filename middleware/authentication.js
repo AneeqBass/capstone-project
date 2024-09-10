7310aec3d6f5e31e5b0bec1b95ee2583678dcc57
Aneeq <aneeqbass@gmail.com>
2024-09-10 18:09:26 +0200

    "Formatted code in usersController.js and authentication.js files, added/removed whitespace and semicolons, and rearranged some logic blocks."

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,9 +1,21 @@
 import { compare } from "bcrypt";
-import { loginUserDb } from "../model/usersDB.js";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
 
 const checkUser = async (req, res, next) => {
     const { email, password } = req.body;
@@ -31,18 +43,5 @@ const checkUser = async (req, res, next) => {
         res.status(500).send("An unexpected error occurred");
     }
 };
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
 
-    if (token == null) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-};
-
-export { authenticateToken };
-export { checkUser };
+export { authenticateToken, checkUser };
